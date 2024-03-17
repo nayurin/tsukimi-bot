79dcb81f06cdc5bc003be932ac1a979f013b0e05
张瑞辰 <ruichen.zhang@chaitin.com>
2024-03-17 19:57:48 +0800

    feat: add error handler

--- src/app.ts
+++ src/app.ts
@@ -11,20 +11,34 @@ const env: Env = loadConfig('.env')
 
   const context = new Context()
   
-  await context.syncLiveStatus(members)
-  console.log(`[${new Date().toLocaleString()}] 加载全部成员直播间状态，成功`)
+  await context.syncLiveStatus(members).then(() => {
+    console.log(`[${new Date().toLocaleString()}] 加载全部成员直播间状态，成功`)
+  }).catch(err => {
+    console.log(`[${new Date().toLocaleString()}] 加载直播间状态失败，退出`)
+    throw err
+  })
   
-  await context.syncDynamicId(members)
-  console.log(`[${new Date().toLocaleString()}] 加载全部成员空间动态，成功`)
+  await context.syncDynamicId(members).then(() => {
+    console.log(`[${new Date().toLocaleString()}] 加载全部成员空间动态，成功`)
+  }).catch(err => {
+    console.log(`[${new Date().toLocaleString()}] 加载成员空间动态失败，退出`)
+    throw err
+  })
   
   while (true) {
     const members: IMember[] = loadConfig('./member.yml')
     
-    await context.syncLiveStatus(members)
-    console.log(`\n[${new Date().toLocaleString()}] live synced`)
+    await context.syncLiveStatus(members).then(() => {
+      console.log(`\n[${new Date().toLocaleString()}] live synced`)
+    }).catch(() => {
+      console.log(`\n[${new Date().toLocaleString()}] live synce failed, waiting for the next try`)
+    })
     
-    await context.syncDynamicId(members)
-    console.log(`[${new Date().toLocaleString()}] dynamic synced`)
+    await context.syncDynamicId(members).then(() => {
+      console.log(`[${new Date().toLocaleString()}] dynamic synced`)
+    }).catch(() => {
+      console.log(`\n[${new Date().toLocaleString()}] dynamic synce failed, waiting for the next try`)
+    })
     
     console.log(inspect(context, false, 100, false))
 
